a3c74a4b53722ad39c4a5b5147c2e2f698a2b813
kevahere <kevahere@gmail.com>
2018-08-20 10:57:30 +0300

    added the showDescription logic

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,Input } from '@angular/core';
+import { Quote } from '../quote'
 @Component({
   selector: 'app-quote-details',
   templateUrl: './quote-details.component.html',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteDetailsComponent implements OnInit {
 
+  @Input() quote:Quote;
   constructor() { }
 
   ngOnInit() {
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,8 @@
 export class Quote {
+  public showDescription:boolean;
   constructor(
     public id: number,
     public name: string,
-    public description:string,) { }
+    public description:string,) {
+      this.showDescription=false}
 }
