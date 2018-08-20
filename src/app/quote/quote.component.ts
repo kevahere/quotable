ab7c3e175347d72b2216b6d86beda0eb9e003d7b
kevahere <kevahere@gmail.com>
2018-08-20 12:15:43 +0300

    added delete functionality

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -7,15 +7,20 @@ import { Quote } from '../quote'
 })
 export class QuoteComponent implements OnInit {
   quotes = [
-    new Quote(1, "he who laughs last","Kevin"),
-    new Quote(1, "simba mwenda pole","Ahere")
+    new Quote(1, "he who laughs last", "Kevin"),
+    new Quote(1, "simba mwenda pole", "Ahere"),
   ]
 
-  toggleDetails(index){
-      this.quotes[index].showDescription = !this.quotes[index].showDescription;
-  }
-  constructor() { }
+  deleteQuote(isComplete,index) {
+    if (isComplete) {
+      this.quotes.splice(index,1);
+    }
+  };
 
+  toggleDetails(index) {
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
+  constructor() {}
   ngOnInit() {
   }
 
