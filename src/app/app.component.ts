2937f2a32056f9453c96b4259ddff787195d7d3f
kevahere <kevahere@gmail.com>
2018-08-20 10:26:08 +0300

    added quote component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
-
+import { Quote } from './quote'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes:string[];
+  quotes = [
+    new Quote(1, "he who laughs last"),
+    new Quote(1, "simba mwenda pole")
+  ]
+  constructor() {
 
-  constructor(){
-    this.quotes =["he who laughs last","Simba mwenda pole",]
   }
 }
