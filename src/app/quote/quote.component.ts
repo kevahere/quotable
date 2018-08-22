a19160b028a5890a1c5ab8a8633e2e600744a03c
kevahere <kevahere@gmail.com>
2018-08-22 07:17:31 +0300

    built prod version and added jumbotron

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,7 +10,7 @@ export class QuoteComponent implements OnInit {
     new Quote(1, "he who laughs last", "Kevin",new Date(2018,6,8)),
     new Quote(1, "simba mwenda pole", "Ahere",new Date(2018,6,8)),
   ]
-
+  //quote:Quote;
   deleteQuote(isComplete,index) {
     if (isComplete) {
       this.quotes.splice(index,1);
@@ -19,6 +19,13 @@ export class QuoteComponent implements OnInit {
 
   toggleDetails(index) {
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
+
+  addNewQuote(quote){
+      let quoteLength = this.quotes.length;
+      quote.id=quoteLength+1;
+      quote.postDate = new Date(quote.postDate)
+      this.quotes.push(quote)
   }
   constructor() {}
   ngOnInit() {
